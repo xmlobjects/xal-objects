d4c1a8f33aff28a4f97da7e7a8557c1b64edc79f
Claus Nagel <cnagel@virtualcitysystems.de>
2020-04-08 14:13:50 +0200

    minor change

--- src/main/java/org/xmlobjects/xal/adapter/XALBuilderHelper.java
+++ src/main/java/org/xmlobjects/xal/adapter/XALBuilderHelper.java
@@ -22,8 +22,8 @@ package org.xmlobjects.xal.adapter;
 import org.xmlobjects.stream.XMLReadException;
 import org.xmlobjects.stream.XMLReader;
 import org.xmlobjects.xal.model.GenericElement;
+import org.xmlobjects.xml.Attribute;
 import org.xmlobjects.xml.Attributes;
-import org.xmlobjects.xml.TextContent;
 
 import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
@@ -33,13 +33,13 @@ import java.util.Map;
 public class XALBuilderHelper {
 
     public static void buildOtherAttributes(Map<QName, String> otherAttributes, Attributes attributes) {
-        for (Map.Entry<QName, TextContent> entry : attributes.toMap().entrySet()) {
-            if (!XMLConstants.NULL_NS_URI.equals(entry.getKey().getNamespaceURI())
-                    && !XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI.equals(entry.getKey().getNamespaceURI())
-                    && !XMLConstants.XML_NS_URI.equals(entry.getKey().getNamespaceURI())
-                    && !XMLConstants.XMLNS_ATTRIBUTE_NS_URI.equals(entry.getKey().getNamespaceURI())
-                    && !XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(entry.getKey().getNamespaceURI()))
-                otherAttributes.put(entry.getKey(), entry.getValue().get());
+        for (Attribute attribute : attributes.toList()) {
+            if (!XMLConstants.NULL_NS_URI.equals(attribute.getNamespaceURI())
+                    && !XMLConstants.W3C_XML_SCHEMA_INSTANCE_NS_URI.equals(attribute.getNamespaceURI())
+                    && !XMLConstants.XML_NS_URI.equals(attribute.getNamespaceURI())
+                    && !XMLConstants.XMLNS_ATTRIBUTE_NS_URI.equals(attribute.getNamespaceURI())
+                    && !XMLConstants.W3C_XML_SCHEMA_NS_URI.equals(attribute.getNamespaceURI()))
+                otherAttributes.put(new QName(attribute.getNamespaceURI(), attribute.getLocalName()), attribute.getValue().get());
         }
     }
 
