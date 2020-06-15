980ca6859de7b7704831dec1ea339b900196eec7
Claus Nagel <cnagel@virtualcitysystems.de>
2020-06-15 16:08:08 +0200

    added walker

--- src/main/java/org/xmlobjects/xal/visitor/XALWalker.java
+++ src/main/java/org/xmlobjects/xal/visitor/XALWalker.java
@@ -19,6 +19,25 @@
 
 package org.xmlobjects.xal.visitor;
 
+import org.xmlobjects.xal.model.Address;
+import org.xmlobjects.xal.model.AddressObject;
+import org.xmlobjects.xal.model.AdministrativeArea;
+import org.xmlobjects.xal.model.Country;
+import org.xmlobjects.xal.model.FreeTextAddress;
+import org.xmlobjects.xal.model.GeoRSS;
+import org.xmlobjects.xal.model.Locality;
+import org.xmlobjects.xal.model.LocationByCoordinates;
+import org.xmlobjects.xal.model.PostCode;
+import org.xmlobjects.xal.model.PostOffice;
+import org.xmlobjects.xal.model.PostalDeliveryPoint;
+import org.xmlobjects.xal.model.Premises;
+import org.xmlobjects.xal.model.RuralDelivery;
+import org.xmlobjects.xal.model.SubAdministrativeArea;
+import org.xmlobjects.xal.model.SubLocality;
+import org.xmlobjects.xal.model.SubPremises;
+import org.xmlobjects.xal.model.SubThoroughfare;
+import org.xmlobjects.xal.model.Thoroughfare;
+
 public abstract class XALWalker implements XALVisitor {
     private boolean shouldWalk = true;
 
@@ -32,5 +51,110 @@ public abstract class XALWalker implements XALVisitor {
 
     public void reset() {
         shouldWalk = true;
+    }
+
+    public void visit(AddressObject addressObject) {
+    }
+
+    @Override
+    public void visit(Address address) {
+        visit((AddressObject) address);
+
+    }
+
+    @Override
+    public void visit(AdministrativeArea administrativeArea) {
+        visit((AddressObject) administrativeArea);
+
+        if (shouldWalk && administrativeArea.getSubAdministrativeArea() != null)
+            administrativeArea.getSubAdministrativeArea().accept(this);
+    }
+
+    @Override
+    public void visit(Country country) {
+        visit((AddressObject) country);
+    }
+
+    @Override
+    public void visit(FreeTextAddress freeTextAddress) {
+        visit((AddressObject) freeTextAddress);
+    }
+
+    @Override
+    public void visit(GeoRSS geoRSS) {
+        visit((AddressObject) geoRSS);
+    }
+
+    @Override
+    public void visit(Locality locality) {
+        visit((AddressObject) locality);
+
+        if (shouldWalk && locality.getSubLocality() != null)
+            locality.getSubLocality().accept(this);
+    }
+
+    @Override
+    public void visit(LocationByCoordinates locationByCoordinates) {
+        visit((AddressObject) locationByCoordinates);
+    }
+
+    @Override
+    public void visit(PostalDeliveryPoint postalDeliveryPoint) {
+        visit((AddressObject) postalDeliveryPoint);
+    }
+
+    @Override
+    public void visit(PostCode postCode) {
+        visit((AddressObject) postCode);
+    }
+
+    @Override
+    public void visit(PostOffice postOffice) {
+        visit((AddressObject) postOffice);
+    }
+
+    @Override
+    public void visit(Premises premises) {
+        visit((AddressObject) premises);
+
+        for (SubPremises subPremises : premises.getSubPremises()) {
+            if (shouldWalk)
+                subPremises.accept(this);
+        }
+    }
+
+    @Override
+    public void visit(RuralDelivery ruralDelivery) {
+        visit((AddressObject) ruralDelivery);
+    }
+
+    @Override
+    public void visit(SubAdministrativeArea subAdministrativeArea) {
+        visit((AddressObject) subAdministrativeArea);
+    }
+
+    @Override
+    public void visit(SubLocality subLocality) {
+        visit((AddressObject) subLocality);
+    }
+
+    @Override
+    public void visit(SubPremises subPremises) {
+        visit((AddressObject) subPremises);
+    }
+
+    @Override
+    public void visit(SubThoroughfare subThoroughfare) {
+        visit((AddressObject) subThoroughfare);
+    }
+
+    @Override
+    public void visit(Thoroughfare thoroughfare) {
+        visit((AddressObject) thoroughfare);
+
+        for (SubThoroughfare subThoroughfare : thoroughfare.getSubThoroughfares()) {
+            if (shouldWalk)
+                subThoroughfare.accept(this);
+        }
     }
 }
