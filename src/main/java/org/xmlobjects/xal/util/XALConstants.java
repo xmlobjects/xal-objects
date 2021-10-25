1827a77a6e759a1fe9beb8977e43a0713041c65e
Claus Nagel <cnagel@virtualcitysystems.de>
2021-10-25 19:48:05 +0200

    added schema locations to XALConstants

--- src/main/java/org/xmlobjects/xal/util/XALConstants.java
+++ src/main/java/org/xmlobjects/xal/util/XALConstants.java
@@ -21,8 +21,11 @@ package org.xmlobjects.xal.util;
 
 public class XALConstants {
     public static final String XAL_3_0_NAMESPACE = "urn:oasis:names:tc:ciq:xal:3";
+    public static final String XAL_3_0_SCHEMA_LOCATION = "http://docs.oasis-open.org/ciq/v3.0/cs02/xsd/default/xsd/xAL.xsd";
     public static final String XAL_3_0_CT_NAMESPACE = "urn:oasis:names:tc:ciq:ct:3";
+    public static final String XAL_3_0_CT_SCHEMA_LOCATION = "http://docs.oasis-open.org/ciq/v3.0/cs02/xsd/default/xsd/CommonTypes.xsd";
     public static final String XAL_2_0_NAMESPACE = "urn:oasis:names:tc:ciq:xsdschema:xAL:2.0";
+    public static final String XAL_2_0_SCHEMA_LOCATION = "http://schemas.opengis.net/citygml/xAL/xAL.xsd";
     public static final String GEORSS_1_1_NAMESPACE = "http://www.georss.org/georss";
     public static final String XLINK_NAMESPACE = "http://www.w3.org/1999/xlink";
 }
